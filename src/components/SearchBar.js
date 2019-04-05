13878d7df62c8679593482abeb3fb382790c561e
Paul Tobin <tobster111@gmail.com>
2019-04-05 14:14:16 +0100

    Added semantic-ui, added onChange + onInputChange

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
-      <div className={SearchBar}>
-        <form>
-          <input type="text" />
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+          <label>Image Search</label>
+            <input type="text" onChange={this.onInputChange}/>
+          </div>
         </form>
       </div>
     );
