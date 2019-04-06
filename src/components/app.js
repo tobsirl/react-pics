27e9c2100530f35f07d1db67fe4ef804cae79638
Paul Tobin <tobster111@gmail.com>
2019-04-06 12:03:01 +0100

    Using async/await syntax for fetching the data

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import dotenv from 'dotenv';
 
 import SearchBar from './SearchBar';
 
+dotenv.config();
+
 class App extends Component {
-  onSubmitSearch(term) {
-    axios.get(`https://api.unsplash.com/search/photos`, {
+  async onSubmitSearch(term) {
+    // const { URL, ACCESS_TOKEN } = process.env;
+    const responce = await axios.get(`https://api.unsplash.com/search/photos`, {
       params: { query: term },
       headers: {
         Authorization: `Client-ID 69f8a965f8ea12b3ccb3d856257a389343332bdb57564efd91c73985efce0058`
       }
     });
+
+    console.log(responce.data.results);
+    return responce;
   }
 
   render() {
