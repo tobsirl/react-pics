6394b102e8d92b16a8787a0d91c94144009b79a1
Paul Tobin <tobster111@gmail.com>
2019-04-06 11:04:21 +0100

    Added axios to make api calls, using headers for authorization

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import SearchBar from './SearchBar';
 
 class App extends Component {
   onSubmitSearch(term) {
-    console.log(term);
+    axios.get(`https://api.unsplash.com/search/photos`, {
+      headers: {
+        Authorization: `Client-ID 69f8a965f8ea12b3ccb3d856257a389343332bdb57564efd91c73985efce0058`
+      }
+    });
   }
 
   render() {
